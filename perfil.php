932ce40606bc93e0e563e92bd60deaa5d968fb13
Julia Barbosa <167313658+JuliaSBarbosa@users.noreply.github.com>
2025-10-25 23:16:10 -0300

    alteracao perfil.php

--- perfil.php
+++ perfil.php
@@ -35,7 +35,35 @@
 
     <!-- Conteúdo principal -->
     <main>
-        <h2 class="titulo">OLÁ, FULANO!</h2>
+        <section class="ficha-medica">
+            <h2>FICHA MÉDICA</h2>
+            <hr>
+            <div class="card-ficha">
+                <div class="perfil">
+                    <img src="img/user-icon.png" alt="Foto do usuário">
+                    <div>
+                        <h3>JOANA DARK</h3>
+                        <p><strong>IDADE:</strong> 23 ANOS</p>
+                    </div>
+                </div>
+
+                <div class="info-basica">
+                    <h4>INFORMAÇÕES BÁSICAS</h4>
+                    <p><strong>DATA DE NASCIMENTO:</strong> 03/04/2002</p>
+                    <p><strong>SEXO:</strong> FEMININO</p>
+                    <p><strong>CPF:</strong> 489.069.228-25</p>
+                    <p><strong>TELEFONE:</strong> (19) 97112-0245</p>
+                    <p><strong>E-MAIL:</strong> JOANA@GMAIL.COM</p>
+                </div>
+
+                <div class="contato-emergencia">
+                    <h4>CONTATO DE EMERGÊNCIA</h4>
+                    <p><strong>CONTATO:</strong> PATRÍCIA</p>
+                    <p><strong>PARENTESCO:</strong> MÃE</p>
+                    <p><strong>TELEFONE:</strong> (19) 99695-1292</p>
+                </div>
+            </div>
+        </section>
     </main>
 
     <!-- Rodapé -->
