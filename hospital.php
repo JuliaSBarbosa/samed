051480844e3d3b2d60ff1c782069aa27f6928a43
Gustavo Marques de Lima <delimagustavomarques@gmail.com>
2025-11-02 18:59:17 -0300

    Adicionando hospitais e refinando o estilo da página

--- hospital.php
+++ hospital.php
@@ -10,7 +10,6 @@
 </head>
 
 <body>
-    <!-- Cabeçalho -->
     <header class="topo">
         <div class="logo">
             <img src="img/logo.svg" alt="Logo SAMED">
@@ -33,14 +32,100 @@
         </a>
     </header>
 
-    <!-- Conteúdo principal -->
     <main>
-        <h2 class="titulo">OLÁ, FULANO!</h2>
+        <h2 class="titulo">HOSPITAIS PRÓXIMOS</h2>
 
+        <div class="hospital-list-container">
+            <ul class="hospital-list">
+                
+                <li class="hospital-card">
+                    <img src="img/logo-sc-itapira.png" alt="Logo Santa Casa de Itapira" class="hospital-logo">
+                    <div class="hospital-info">
+                        <div class="hospital-title">
+                            <h3>SANTA CASA DE ITAPIRA</h3>
+                            <span class="city-tag">Itapira - SP</span>
+                        </div>
+                        <p>
+                            <strong>Endereço:</strong> 
+                            <a href="https://www.google.com/maps/search/?api=1&query=Rua+Cel.+Jose+Pires,138,Itapira,SP" target="_blank">
+                                R. Cel. José Píres, 138 - Centro, Itapira - SP, 13970-060
+                            </a>
+                        </p>
+                        <p><strong>Telefone:</strong> (19) 3863-1122</p>
+                    </div>
+                </li>
 
+                <li class="hospital-card">
+                    <img src="img/logo-hm-itapira.png" alt="Logo Hospital Municipal de Itapira" class="hospital-logo">
+                    <div class="hospital-info">
+                        <div class="hospital-title">
+                            <h3>HOSPITAL MUNICIPAL DE ITAPIRA</h3>
+                            <span class="city-tag">Itapira - SP</span>
+                        </div>
+                        <p>
+                            <strong>Endereço:</strong>
+                            <a href="https://www.google.com/maps/search/?api=1&query=Rua+Farmaceutico+Pires+de+Godoi,313,Itapira,SP" target="_blank">
+                                R. Farm. Píres de Godói, 313 - Centro, Itapira - SP, 13970-190
+                            </a>
+                        </p>
+                        <p><strong>Telefone:</strong> (19) 3843-1222</p>
+                    </div>
+                </li>
+                
+                <li class="hospital-card">
+                    <img src="img/logo-sf.png" alt="Logo Hospital São Francisco" class="hospital-logo">
+                    <div class="hospital-info">
+                        <div class="hospital-title">
+                            <h3>HOSPITAL SÃO FRANCISCO</h3>
+                            <span class="city-tag">Mogi Guaçu - SP</span>
+                        </div>
+                        <p>
+                            <strong>Endereço:</strong> 
+                            <a href="https://www.google.com/maps/search/?api=1&query=Rua+Inacio+Franco+Alves,561,Mogi+Guacu,SP" target="_blank">
+                                R. Inácio Franco Alves, 561 - Parque Cidade Nova, Mogi Guaçu - SP, 13845-420
+                            </a>
+                        </p>
+                        <p><strong>Telefone:</strong> (19) 3851-8000</p>
+                    </div>
+                </li>
+
+                <li class="hospital-card">
+                    <img src="img/logo-sc.png" alt="Logo Hospital Santa Casa" class="hospital-logo">
+                    <div class="hospital-info">
+                        <div class="hospital-title">
+                            <h3>HOSPITAL SANTA CASA</h3>
+                            <span class="city-tag">Mogi Guaçu - SP</span>
+                        </div>
+                        <p>
+                            <strong>Endereço:</strong>
+                            <a href="https://www.google.com/maps/search/?api=1&query=Rua+Chico+de+Paula,608,Mogi+Guacu,SP" target="_blank">
+                                R. Chico de Paula, 608 - Centro, Mogi Guaçu - SP, 13840-005
+                            </a>
+                        </p>
+                        <p><strong>Telefone:</strong> (19) 3861-1313</p>
+                    </div>
+                </li>
+
+                <li class="hospital-card">
+                    <img src="img/logo-h22.png" alt="Logo Hospital 22 de Outubro" class="hospital-logo">
+                    <div class="hospital-info">
+                        <div class="hospital-title">
+                            <h3>HOSPITAL 22 DE OUTUBRO</h3>
+                            <span class="city-tag">Mogi Mirim - SP</span>
+                        </div>
+                        <p>
+                            <strong>Endereço:</strong>
+                            <a href="https://www.google.com/maps/search/?api=1&query=Av+22+de+Outubro,733,Mogi+Mirim,SP" target="_blank">
+                                Av. 22 de Outubro, 733 - Jardim Santa Helena, Mogi Mirim - SP, 13806-050
+                            </a>
+                        </p>
+                        <p><strong>Telefone:</strong> (19) 3814-3400</p>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
     </main>
-
-    <!-- Rodapé -->
     <footer>
         <div class="footer-logo">
             <img src="img/logo.svg" alt="Logo SAMED">
