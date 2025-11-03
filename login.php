b1723628923f3835b8952bd9a1191a43cb1289d1
AmabileSilverio <amabile.silverio34@gmail.com>
2025-11-03 12:02:53 -0300

    login.php
    Criando tela de login

---
+++ login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAMED - Início</title>
+    <link rel="stylesheet" href="estilos/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Magra:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="img/logo.png" type="image/png">
+</head>
+
+<body>
+    <!-- Cabeçalho -->
+    <header class="topo">
+        <div class="logo">
+            <img src="img/logo.svg" alt="Logo SAMED">
+            <h1>SAMED</h1>
+        </div>
+
+        <div class="menu-actions">
+            <nav class="menu">
+                <a href="index.php" class="ativo">INÍCIO</a>
+                <span class="divisor">|</span>
+                <a href="perfil.php">MEU PERFIL</a>
+                <span class="divisor">|</span>
+                <a href="dependentes.php">DEPENDENTES</a>
+                <span class="divisor">|</span>
+                <a href="hospital.php">UNIDADES DE SAÚDE</a>
+            </nav>
+
+            <a href="login.php" class="botao-login">
+                LOGIN
+            </a>
+            <a href="registrar.php" class="botao-registrar">
+                REGISTRE-SE
+            </a>
+        </div>
+    </header>
+    
+    <main class="hero">
+  <div class="login-container">
+    <h2>LOGIN</h2>
+    <br>
+    <form action="login_process.php" method="post">
+      <label for="username">NOME DO USUÁRIO</label>
+      <input type="text" id="username" name="username" required placeholder="Digite seu usuário">
+
+      <label for="password">SENHA: </label>
+      <input type="password" id="password" name="password" required placeholder="Digite sua senha">
+        <br>
+      <input type="submit" value="ENTRAR">
+    </form>
+  </div>
+</main>
+
+</body>
+
+</html>
