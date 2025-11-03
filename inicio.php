bbd84ef0a4180f6b7f845e3748ce30eaa65ef317
AmabileSilverio <amabile.silverio34@gmail.com>
2025-11-03 12:02:30 -0300

    inicio.php
    Criado tela inicio.php

---
+++ inicio.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAMED - Início</title>
+    <link rel="stylesheet" href="estilos/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Magra:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="img/logo.png" type="image/png">
+</head>
+
+<body>
+    <!-- Cabeçalho -->
+    <header class="topo">
+        <div class="logo">
+            <img src="img/logo.svg" alt="Logo SAMED">
+            <h1>SAMED</h1>
+        </div>
+
+        <div class="menu-actions">
+            <nav class="menu">
+                <a href="index.php" class="ativo">INÍCIO</a>
+                <span class="divisor">|</span>
+                <a href="perfil.php">MEU PERFIL</a>
+                <span class="divisor">|</span>
+                <a href="dependentes.php">DEPENDENTES</a>
+                <span class="divisor">|</span>
+                <a href="hospital.php">UNIDADES DE SAÚDE</a>
+            </nav>
+
+            <a href="login.php" class="botao-login">
+                LOGIN
+            </a>
+            <a href="registrar.php" class="botao-registrar">
+                REGISTRE-SE
+            </a>
+        </div>
+    </header>
+    <main class="hero">
+        <div class="hero-content">
+            <h2 class="hero-title">CUIDANDO DE QUEM VOCÊ AMA,</h2>
+                <h2 class="hero-title">MESMO QUANDO VOCÊ NÃO PODE FALAR</h2>
+            <p class="hero-subtitle">Serviços de saúde integrados para toda a família</p>
+        </div>
+    </main>
+
+</body>
+
+</html>
